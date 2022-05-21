f80bd07575c5b054b889e65b79d06f85f107506c
John-Thailand <hamoban69@gmail.com>
2022-05-21 15:53:44 +0900

    BookShelf.java modified

--- BookShelf.java
+++ BookShelf.java
@@ -1,19 +1,18 @@
 import java.util.Iterator;
+import java.util.ArrayList;
 
 public class BookShelf {
-  private Book[] books;
-  private int last = 0;
+  private ArrayList<Book> books;
 
-  public BookShelf(int maxsize) { this.books = new Book[maxsize]; }
+  public BookShelf(int maxsize) { this.books =  new ArrayList<>(); }
 
-  public Book getBookAt(int index) { return books[index]; }
+  public Book getBookAt(int index) { return books.get(index); }
 
   public void appendBook(Book book) {
-    this.books[last] = book;
-    last++;
+    this.books.add(book);
   }
 
-  public int getLength() { return last; }
+  public int getLength() { return books.size(); }
 
   public Iterator<Book> iterator() {
     return new BookShelfIterator(this);
